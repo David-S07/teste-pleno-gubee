36424c4f607ac7edff4385f993dea56a5164304f
David-S07 <david.tata@hotmail.com>
2023-08-04 23:11:25 -0300

    adicionando model

--- model/src/main/java/br/com/gubee/interview/model/Hero.java
+++ model/src/main/java/br/com/gubee/interview/model/Hero.java
@@ -1,4 +1,72 @@
 package br.com.gubee.interview.model;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class Hero {
+
+    private UUID id;
+    private String name;
+    private String race;
+    private UUID powerStatsId;
+    private boolean enabled;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public UUID getPowerStatsId() {
+        return powerStatsId;
+    }
+
+    public void setPowerStatsId(UUID powerStatsId) {
+        this.powerStatsId = powerStatsId;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
